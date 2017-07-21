7f4ff0ed7c3089634ec404febb9524041587ce01
Serj Telepov <serjtelepov@gmail.com>
2017-07-21 12:23:05 +0300

    +gulp-usebem for html

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,36 @@ var precss = require("precss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var gulpUsebem = require('gulp-usebem');
+var gulpIf = require('gulp-if');
+var gulpIgnore = require('gulp-ignore');
+
+gulp.task('bem', function () {
+  var bemAssets = gulpUsebem.assets({
+    blockPaths: ['app/blocks'],
+    exts: ['css']
+  });
+ 
+  return gulp.src('app/*.html')
+    .pipe(bemAssets)
+    // Add assets to pipe 
+    // Save css files 
+    .pipe(gulpIf(
+      '*.css',
+      gulp.dest('dist/styles/bem')
+    ))
+    // Exclude assets with default paths 
+    .pipe(gulpIgnore.exclude('**/*'))
+    // Restore app/*.html 
+    .pipe(bemAssets.restore())
+    // Replace markers in HTML 
+    .pipe(gulpUsebem({
+      cssCommentMarker: 'bem-css',
+      cssBlockPath: 'styles/bem'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
     .pipe(plumber())
